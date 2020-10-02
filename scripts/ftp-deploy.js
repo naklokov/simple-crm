555c0b4a0287e06a966577ccbfc0449b41d5a27a
ukropchik <ukropchik.petrushechka@gmail.com>
2020-10-02 23:31:58 +0300

    fix: Добавить маску для добавочного номера в поле ввода телефона
    [OLVE-248] - Сделать несортируемым поле "Куратор"
    
    Добавить скрипт для деплоя (пока не работает коннект)

---
+++ scripts/ftp-deploy.js
@@ -0,0 +1,30 @@
+var FtpDeploy = require("ftp-deploy");
+var ftpDeploy = new FtpDeploy();
+
+var config = {
+  user: "front",
+  // Password optional, prompted if none given
+  password: "front123",
+  host: "176.212.190.167",
+  port: 21,
+  localRoot: "../build",
+  remoteRoot: "/nginx/html",
+  include: ["*", "**/*"], // this would upload everything except dot files
+  // include: ["*.php", "dist/*", ".*"],
+  // e.g. exclude sourcemaps, and ALL files in node_modules (including dot files)
+  exclude: [
+    "dist/**/*.map",
+    "node_modules/**",
+    "node_modules/**/.*",
+    ".git/**",
+  ],
+  // delete ALL existing files at destination before uploading, if true
+  deleteRemote: true,
+  // Passive mode is forced (EPSV command is not sent)
+  forcePasv: true,
+};
+
+ftpDeploy
+  .deploy(config)
+  .then((res) => console.log("finished:", res))
+  .catch((err) => console.log(err));
