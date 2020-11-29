cc651c88a8fcbb3e01699a667db3b38242b0240c
ukropchik <ukropchik.petrushechka@gmail.com>
2020-11-30 00:36:01 +0300

    chore: Исправить скрипт деплоя на дев сервер

--- scripts/dev-ftp-deploy.js
+++ scripts/dev-ftp-deploy.js
@@ -1,25 +1,39 @@
-var FtpDeploy = require("ftp-deploy");
-var ftpDeploy = new FtpDeploy();
+const SftpClient = require("ssh2-sftp-client");
+const path = require("path");
 
-var config = {
-  user: "test",
-  password: "test123",
-  host: "176.212.190.167",
-  port: 31,
-  localRoot: "build/",
-  remoteRoot: "/nginx/www/crm",
-  include: ["*", "**/*"],
-  exclude: [
-    "dist/**/*.map",
-    "node_modules/**",
-    "node_modules/**/.*",
-    ".git/**",
-  ],
-  deleteRemote: true,
-  forcePasv: false,
+const dotenvPath = path.join(__dirname, "..", ".env");
+require("dotenv").config({ path: dotenvPath });
+
+const config = {
+  host: "77.222.60.155",
+  username: "root",
+  password: "xavutiiziw",
+  port: 22,
 };
 
-ftpDeploy
-  .deploy(config)
-  .then((res) => console.log("dev has been published:", res))
-  .catch((err) => console.log(err));
+async function main() {
+  const client = new SftpClient("upload-test");
+  console.log(__dirname);
+  const src = path.join(__dirname, "..", "build");
+  const dst = "/var/www/crm";
+
+  try {
+    await client.connect(config);
+    client.on("upload", (info) => {
+      console.log(`Listener: Uploaded ${info.source}`);
+    });
+    let rslt = await client.uploadDir(src, dst);
+    return rslt;
+  } finally {
+    client.end();
+  }
+}
+
+main()
+  .then((msg) => {
+    console.log("\x1b[32m%s\x1b[0m", `UPLOAD SUCCESSFUL`);
+    console.log("\x1b[32m%s\x1b[0m", msg);
+  })
+  .catch((err) => {
+    console.log("\x1b[31m%s\x1b[0m", `ERROR ON UPLOAD: ${err.message}`);
+  });
