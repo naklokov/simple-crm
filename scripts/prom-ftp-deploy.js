3e212b6b6ae1a007e1ffa4fa14b7cd7489c5db1b
ukropchik <ukropchik.petrushechka@gmail.com>
2021-02-10 16:23:44 +0300

    chore: Добавить конфиг деплоя для ПРОМа

--- scripts/prom-ftp-deploy.js
+++ scripts/prom-ftp-deploy.js
@@ -1,33 +1,39 @@
-var FtpDeploy = require("ftp-deploy");
-var ftpDeploy = new FtpDeploy();
+const SftpClient = require("ssh2-sftp-client");
+const path = require("path");
 
-var config = {
-  user: "prom",
-  // Password optional, prompted if none given
-  password: "prom123",
-  // external
-  host: "176.212.190.167",
-  // internal
-  // host: "176.212.190.167",
-  port: 21,
-  localRoot: "build/",
-  remoteRoot: "/nginx-pl/www/crm",
-  include: ["*", "**/*"], // this would upload everything except dot files
-  // include: ["*.php", "dist/*", ".*"],
-  // e.g. exclude sourcemaps, and ALL files in node_modules (including dot files)
-  exclude: [
-    "dist/**/*.map",
-    "node_modules/**",
-    "node_modules/**/.*",
-    ".git/**",
-  ],
-  // delete ALL existing files at destination before uploading, if true
-  deleteRemote: true,
-  // Passive mode is forced (EPSV command is not sent)
-  forcePasv: false,
+const dotenvPath = path.join(__dirname, "..", ".env");
+require("dotenv").config({ path: dotenvPath });
+
+const config = {
+  host: "176.118.220.215",
+  username: "admn",
+  password: "olvje2021",
+  port: 22,
 };
 
-ftpDeploy
-  .deploy(config)
-  .then((res) => console.log("prom has been published:", res))
-  .catch((err) => console.log(err));
+async function main() {
+  const client = new SftpClient("upload-test");
+  console.log(__dirname);
+  const src = path.join(__dirname, "..", "build");
+  const dst = "/var/www/crm";
+
+  try {
+    await client.connect(config);
+    client.on("upload", (info) => {
+      console.log(`Listener: Uploaded ${info.source}`);
+    });
+    let rslt = await client.uploadDir(src, dst);
+    return rslt;
+  } finally {
+    client.end();
+  }
+}
+
+main()
+  .then((msg) => {
+    console.log("\x1b[32m%s\x1b[0m", `UPLOAD SUCCESSFUL`);
+    console.log("\x1b[32m%s\x1b[0m", msg);
+  })
+  .catch((err) => {
+    console.log("\x1b[31m%s\x1b[0m", `ERROR ON UPLOAD: ${err.message}`);
+  });
