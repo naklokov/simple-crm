92385888093ce35524ff79cf22d10d21f8e51b7b
Sergey <kapcov@yandex.ru>
2021-04-08 00:44:03 +0300

    feature: Вынес визуализацию дропдауна сортировки в компонент [OLVE-487]

--- src/forms/client-card/tabs/comments/comments.tsx
+++ src/forms/client-card/tabs/comments/comments.tsx
@@ -1,32 +1,33 @@
-import React, { useState, useEffect, useCallback, useRef } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import axios from "axios";
-import { Dropdown, List, Menu } from "antd";
+import { List } from "antd";
 import { orderBy } from "lodash";
 import { useTranslation } from "react-i18next";
 import { useParams } from "react-router-dom";
 import { connect } from "react-redux";
-import { DownOutlined } from "@ant-design/icons";
 import { Comment } from "../../../../components";
-import { Footer } from "./components";
+import { Footer, Order } from "./components";
 import {
   defaultErrorHandler,
-  getFullUrl,
   getFiteredEntityArray,
+  getFullUrl,
+  getRsqlParams,
   getUpdatedEntityArray,
   useFetch,
-  getRsqlParams,
 } from "../../../../utils";
 import {
-  urls,
   CommentEntityProps,
+  ProfileInfoEntityProps,
   QueryProps,
   State,
-  ProfileInfoEntityProps,
   TabPaneFormProps,
+  urls,
 } from "../../../../constants";
 import { getPostData } from "./utils";
 
 import style from "./comments.module.scss";
+
+type TDirection = "asc" | "desc";
 
 interface CommentsProps extends TabPaneFormProps {
   profileInfo: ProfileInfoEntityProps;
@@ -35,8 +36,9 @@ interface CommentsProps extends TabPaneFormProps {
 export const Comments = ({ profileInfo }: CommentsProps) => {
   const [loading, setLoading] = useState(false);
   const [comments, setComments] = useState([] as CommentEntityProps[]);
-  const [order, setOrder] = useState<"asc" | "desc">("asc");
+  const [order, setOrder] = useState<TDirection>("asc");
   const listRef = useRef<HTMLDivElement>(null);
+  const COMMENT_DATE_FIELD_CODE = "creationDate";
 
   const { id: entityId } = useParams<QueryProps>();
   const [t] = useTranslation("clientCardComments");
@@ -130,38 +132,14 @@ export const Comments = ({ profileInfo }: CommentsProps) => {
     [comments, entityId, profileInfo.id, t]
   );
 
-  const orderLabel = order === "asc" ? "Сначала старые" : "Сначала новые";
-
-  const handleOrderComment = (direction: "asc" | "desc") => {
-    setOrder(direction);
-  };
-  const menu = (
-    <Menu>
-      <Menu.Item key="asc" onClick={() => handleOrderComment("asc")}>
-        Старые
-      </Menu.Item>
-      <Menu.Item key="desc" onClick={() => handleOrderComment("desc")}>
-        Новые
-      </Menu.Item>
-    </Menu>
-  );
-
   return (
     <div className={style.container}>
-      <Dropdown overlay={menu} trigger={["click"]}>
-        <a
-          href="#"
-          className="ant-dropdown-link"
-          onClick={(e) => e.preventDefault()}
-        >
-          {orderLabel} <DownOutlined />
-        </a>
-      </Dropdown>
+      <Order onChange={setOrder} value={order} />
       <div ref={listRef} className={style.list}>
         <List
           loading={loading}
           itemLayout="horizontal"
-          dataSource={orderBy(comments, "creationDate", order)}
+          dataSource={orderBy(comments, COMMENT_DATE_FIELD_CODE, order)}
           renderItem={(comment) => (
             <List.Item>
               <Comment
